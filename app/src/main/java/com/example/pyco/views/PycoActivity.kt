7f2dc6759c5dcbb3e604fd51f29d46269818d225
Lasse Herzog <lasse@herzoeglich.de>
2024-03-04 17:28:40 +0100

    integrated Hilt into the main activity

--- app/src/main/java/com/example/pyco/views/PycoActivity.kt
+++ app/src/main/java/com/example/pyco/views/PycoActivity.kt
@@ -8,8 +8,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.pyco.views.ui.theme.PycoTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+/**
+ * This is the Main activity for the Pyco app.
+ */
+@AndroidEntryPoint
+class PycoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
