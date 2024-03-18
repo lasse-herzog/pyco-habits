160dc0707562be8941299f972a9531cbf4b5cc56
Daniel Schwager <dschwager@exone.de>
2024-03-18 14:14:45 +0100

    added go back button

--- app/src/main/java/com/example/pyco/views/HabitDetailsView.kt
+++ app/src/main/java/com/example/pyco/views/HabitDetailsView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,9 +16,19 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,10 +39,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.pyco.R
 import com.example.pyco.views.ui.theme.PycoTheme
 
@@ -47,104 +61,126 @@ data class HabitDetails(
 val colorLightOrange = Color(0xFFFFE0B9)
 val colorLightOrange2 = Color(0xFFFFA0B1)
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HabitDetailsView(habitDetails: HabitDetails) {
-    Surface(
-        shape = MaterialTheme.shapes.medium,
-        shadowElevation = 3.dp,
-        modifier = Modifier.padding(4.dp)
-    ) {
-
-        Column(modifier = Modifier.background(colorLightOrange)) {
-            Text(
-                text = habitDetails.title,
-                color = Color.Black,
-                fontSize = 30.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .padding(top = 10.dp, bottom = 20.dp)
-                    .fillMaxWidth()
-            )
-
-            Row {
-                Column(
-                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 7.dp),
-                ) {
-                    Image(
-                        painter = painterResource(R.mipmap.ic_habit_icon),
-                        contentDescription = "Placeholder icon",
-                        modifier = Modifier
-                            .size(70.dp)
-                            .clip(CircleShape)
-                            .shadow(
-                                elevation = 1.dp,
-                                shape = CircleShape,
-                                ambientColor = MaterialTheme.colorScheme.surface
-                            )
+fun HabitDetailsView(habitDetails: HabitDetails, navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        "Detailansicht",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
                     )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigateUp() }) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Zurück")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier.background(colorLightOrange).padding(innerPadding).fillMaxHeight()) {
+                Text(
+                    text = habitDetails.title,
+                    color = Color.Black,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(top = 20.dp, bottom = 20.dp)
+                        .fillMaxWidth()
+                )
+
+                Row {
+                    Column(
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 7.dp),
+                    ) {
+                        Image(
+                            painter = painterResource(R.mipmap.ic_habit_icon),
+                            contentDescription = "Placeholder icon",
+                            modifier = Modifier
+                                .size(70.dp)
+                                .clip(CircleShape)
+                                .shadow(
+                                    elevation = 1.dp,
+                                    shape = CircleShape,
+                                    ambientColor = MaterialTheme.colorScheme.surface
+                                )
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(25.dp))
+
+                    Box(
+                        modifier = Modifier
+                            .background(
+                                color = colorLightOrange2,
+                                shape = RoundedCornerShape(30.dp)
+                            )
+                    ) {
+
+                        Column {
+                            Text(
+                                text = "Tags:",
+                                color = Color.White,
+                                fontSize = 30.sp,
+                                modifier = Modifier.padding(horizontal = 40.dp)
+                            )
+                            Text(
+                                text = habitDetails.tags.component1(),
+                                color = Color.White,
+                                modifier = Modifier.padding(horizontal = 70.dp)
+                            )
+                            Text(
+                                text = habitDetails.tags.component2(),
+                                color = Color.White,
+                                modifier = Modifier.padding(horizontal = 70.dp)
+                            )
+                        }
+                    }
                 }
 
-                Spacer(modifier = Modifier.width(25.dp))
-
-                Box(
+                Text(
+                    text = "Habit Details:",
+                    color = Color.Black,
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
                     modifier = Modifier
-                        .background(
-                            color = colorLightOrange2,
-                            shape = RoundedCornerShape(30.dp)
-                        )
-                ) {
+                        .padding(top = 20.dp, bottom = 20.dp)
+                        .fillMaxWidth()
+                )
+                Column(Modifier.padding(20.dp)) {
 
-                    Column {
+                    Box(
+                        modifier = Modifier
+                            .background(
+                                color = colorLightOrange2,
+                                shape = RoundedCornerShape(20.dp)
+                            )
+                            .padding(30.dp)
+
+                    ) {
+
                         Text(
-                            text = "Tags:",
+                            text = habitDetails.details,
                             color = Color.White,
-                            fontSize = 30.sp,
-                            modifier = Modifier.padding(horizontal = 40.dp)
-                        )
-                        Text(
-                            text = habitDetails.tags.component1(),
-                            color = Color.White,
-                            modifier = Modifier.padding(horizontal = 70.dp)
-                        )
-                        Text(
-                            text = habitDetails.tags.component2(),
-                            color = Color.White,
-                            modifier = Modifier.padding(horizontal = 70.dp)
+                            fontSize = 15.sp,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .padding(20.dp)
+
                         )
                     }
                 }
             }
-
-            Text(
-                text = "Habit Details:",
-                color = Color.Black,
-                fontSize = 30.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .padding(top = 20.dp, bottom = 20.dp)
-                    .fillMaxWidth()
-            )
-            Box(
-                modifier = Modifier
-                    .background(
-                        color = colorLightOrange2,
-                        shape = RoundedCornerShape(30.dp)
-                    ),
-            ) {
-
-                Text(
-                    text = habitDetails.details,
-                    color = Color.White,
-                    fontSize = 15.sp,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .padding(20.dp)
-                        .fillMaxWidth()
-                )
-            }
         }
     }
-}
 
 @Preview
 @Composable
@@ -159,7 +195,7 @@ fun DefaultPreview() {
                     "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
                     listOf("Leben", "Ernärung"),
                     3
-                )
+                ), navController = rememberNavController()
             )
         }
     }
--- app/src/main/java/com/example/pyco/views/PycoNavigationBar.kt
+++ app/src/main/java/com/example/pyco/views/PycoNavigationBar.kt
@@ -94,7 +94,7 @@ fun PycoNavigationBar() {
                         "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.",
                         listOf("Leben", "Ernärung"),
                         3
-                    )
+                    ), navController
                 )
             }
         }
