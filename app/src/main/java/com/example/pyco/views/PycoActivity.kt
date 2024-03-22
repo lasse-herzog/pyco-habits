0eeea95dc70b7354331d0bcecd2f6f4cfa32918d
Lasse Herzog <lasse@herzoeglich.de>
2024-03-22 22:55:58 +0100

    enable edge to edge

--- app/src/main/java/com/example/pyco/views/PycoActivity.kt
+++ app/src/main/java/com/example/pyco/views/PycoActivity.kt
@@ -3,10 +3,7 @@ package com.example.pyco.views
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import androidx.activity.enableEdgeToEdge
 import com.example.pyco.views.ui.theme.PycoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,16 +13,13 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class PycoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+
         super.onCreate(savedInstanceState)
+
         setContent {
             PycoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    PycoNavigationBar()
-                }
+                PycoNavigationBar()
             }
         }
     }
