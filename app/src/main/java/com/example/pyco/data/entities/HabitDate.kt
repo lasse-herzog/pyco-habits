4b22dbbdb57d3f77554255450157d6e9451b6f51
Lasse Herzog <lasse@herzoeglich.de>
2024-03-23 16:01:17 +0100

    updated data model for HabitDate

--- app/src/main/java/com/example/pyco/data/entities/HabitDate.kt
+++ app/src/main/java/com/example/pyco/data/entities/HabitDate.kt
@@ -3,9 +3,9 @@ package com.example.pyco.data.entities
 import androidx.room.Entity
 import java.time.LocalDate
 
-@Entity(tableName = "habitDate", primaryKeys = ["habitId","date"])
-data class HabitDate (
-    val habitId : Int,
-    val date : LocalDate,
-    val habitDone: Boolean
+@Entity(tableName = "habitDate", primaryKeys = ["habitId", "date"])
+data class HabitDate(
+    val habitId: Int,
+    val date: LocalDate,
+    val habitPracticed: Boolean? = null
 )
