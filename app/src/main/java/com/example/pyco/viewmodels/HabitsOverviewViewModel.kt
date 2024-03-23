597bf0874c6c7f775883869da44c280f5fdac894
Lasse Herzog <lasse@herzoeglich.de>
2024-03-23 17:29:58 +0100

    update HabitsOverview to match new repository locations

--- app/src/main/java/com/example/pyco/viewmodels/HabitsOverviewViewModel.kt
+++ app/src/main/java/com/example/pyco/viewmodels/HabitsOverviewViewModel.kt
@@ -1,21 +1,14 @@
 package com.example.pyco.viewmodels
 
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.toMutableStateList
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.pyco.data.HabitBlueprintsRepository
-import com.example.pyco.data.HabitsRepository
 import com.example.pyco.data.entities.Habit
-import com.example.pyco.data.entities.HabitAndHabitBlueprint
 import com.example.pyco.data.entities.HabitAndHabitBlueprintWithCategories
+import com.example.pyco.data.repositories.HabitBlueprintsRepository
+import com.example.pyco.data.repositories.HabitsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
