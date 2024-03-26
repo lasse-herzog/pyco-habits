0b04496fbf81bf300ba7c79659220e15620bbbd4
Lasse Herzog <lasse@herzoeglich.de>
2024-03-26 12:57:26 +0100

    changed icon of streak in navbar and added calendar to navbar

--- app/src/main/java/com/example/pyco/views/PycoNavigationBar.kt
+++ app/src/main/java/com/example/pyco/views/PycoNavigationBar.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -23,6 +24,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.pyco.R
 import com.example.pyco.views.navigation.Screen
 import com.example.pyco.views.streak.StreakView
 import com.example.pyco.views.ui.theme.PycoTheme
@@ -36,7 +38,11 @@ data class NavBarItem(val screen: Screen, val icon: ImageVector)
 fun PycoNavigationBar() {
     val navController = rememberNavController()
     val navBarItems = listOf(
-        NavBarItem(Screen.Streak, Icons.Filled.DateRange),
+        NavBarItem(Screen.Calendar, Icons.Filled.DateRange),
+        NavBarItem(
+            Screen.Streak,
+            ImageVector.vectorResource(id = R.drawable.baseline_bar_chart_24)
+        ),
         NavBarItem(Screen.Home, Icons.Filled.Home),
         NavBarItem(Screen.Habits, Icons.AutoMirrored.Filled.List),
     )
@@ -79,6 +85,7 @@ fun PycoNavigationBar() {
                 .padding(innerPadding)
                 .consumeWindowInsets(innerPadding)
         ) {
+            composable(Screen.Calendar.route) { PycoCalendarScreen() }
             composable(Screen.Streak.route) { StreakView() }
             composable(Screen.Habits.route) { HabitsOverviewScreen() }
             composable(Screen.Home.route) { PycoHomeScreen() }
--- app/src/main/java/com/example/pyco/views/navigation/Screen.kt
+++ app/src/main/java/com/example/pyco/views/navigation/Screen.kt
@@ -4,7 +4,8 @@ import androidx.annotation.StringRes
 import com.example.pyco.R
 
 sealed class Screen(val route: String, @StringRes val resourceId: Int) {
-    data object Streak : Screen("streak", R.string.streak)
-    data object Home : Screen("home", R.string.home)
+    data object Calendar : Screen("calendar", R.string.calendar)
     data object Habits : Screen("habits", R.string.habits)
+    data object Home : Screen("home", R.string.home)
+    data object Streak : Screen("streak", R.string.streak)
 }
