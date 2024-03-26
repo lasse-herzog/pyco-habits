e3a375f397b76244a72d59c761b4da43bed1150d
Jonas Wiedenmann <jwiedenmann@exone.de>
2024-03-26 17:10:25 +0100

    finished work on worker

--- app/src/main/java/com/example/pyco/views/PycoActivity.kt
+++ app/src/main/java/com/example/pyco/views/PycoActivity.kt
@@ -1,13 +1,19 @@
 package com.example.pyco.views
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.pyco.data.repositories.HabitsRepository
 import com.example.pyco.views.ui.theme.PycoTheme
+import com.example.pyco.worker.HabitWorker
 import dagger.Module
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 /**
@@ -29,6 +35,18 @@ class PycoActivity :
     }
 
     override fun onStart() {
+        runHabitWorker(this.applicationContext)
         super.onStart()
+    }
+
+    override fun onResume() {
+        runHabitWorker(this.applicationContext)
+        super.onResume()
+    }
+
+    private fun runHabitWorker(applicationContext: Context) {
+        runBlocking {
+            HabitWorker(applicationContext).doWork()
+        }
     }
 }
--- app/src/main/java/com/example/pyco/worker/DailyHabitWorker.kt
+++ app/src/main/java/com/example/pyco/worker/DailyHabitWorker.kt
@@ -3,53 +3,16 @@ package com.example.pyco.worker
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.example.pyco.data.entities.HabitDate
-import com.example.pyco.data.repositories.HabitsRepository
-import java.time.LocalDate
 import javax.inject.Inject
 
 class DailyHabitWorker @Inject constructor(
-    appContext: Context,
-    workerParams: WorkerParameters,
-    val habitsRepository: HabitsRepository
+    private val appContext: Context,
+    workerParams: WorkerParameters
 ) :
     CoroutineWorker(appContext, workerParams) {
 
     override suspend fun doWork(): Result {
-        val habits = habitsRepository.getHabits()
-        val localDate = LocalDate.now()
-
-        for (habit in habits) {
-            val habitDate: HabitDate? = habitsRepository.getLastHabitDate(habit)
-
-            // if null create one for today
-            if (habitDate == null) {
-                habitsRepository.createHabitDate(habit.habitId, localDate)
-                continue
-            }
-
-            // check if habitdate is before today, exit if today
-            if (habitDate.date == localDate) continue
-
-            // if habitdate practiced == null, set to undone
-            if (habitDate.habitPracticed == null) {
-                habitsRepository.createHabitDate(habit.habitId, habitDate.date, false)
-            }
-
-            // find the next due date
-            var nextHabitDate = habitDate.date.plusDays(habit.interval.toLong())
-            while (nextHabitDate < localDate) {
-                // if next due day < today, create habit date and set practiced false
-                habitsRepository.createHabitDate(habit.habitId, nextHabitDate, false)
-                nextHabitDate = nextHabitDate.plusDays(habit.interval.toLong())
-            }
-
-            // if next due day = today, create habit date and set practiced null
-            if (nextHabitDate == localDate) {
-                habitsRepository.createHabitDate(habit.habitId, nextHabitDate)
-            }
-        }
-
-        return Result.failure()
+        HabitWorker(appContext).doWork()
+        return Result.success()
     }
 }
